34b613086aacd4f14493de3c0e0d1753d6039f75
Manfredi Christian <c.manfredi.28@gmail.com>
2015-03-12 11:33:06 +0100

    buona

---
+++ src/interfacce/CardPanel.java
@@ -0,0 +1,30 @@
+package interfacce;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Christian on 12/03/2015.
+ */
+public class CardPanel extends JPanel{
+
+    private Image img;
+
+    public CardPanel(String img) {
+        this(new ImageIcon(img).getImage());
+    }
+
+    public CardPanel(Image img) {
+        this.img = img;
+        Dimension size = new Dimension(img.getWidth(null), img.getHeight(null));
+        setPreferredSize(size);
+        setMinimumSize(size);
+        setMaximumSize(size);
+        setSize(size);
+        setLayout(null);
+    }
+
+    public void paintComponent(Graphics g) {
+        g.drawImage(img, 0, 0, null);
+    }
+}
