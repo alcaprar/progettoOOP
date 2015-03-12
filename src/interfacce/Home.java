237c88b103a6b52a834d9ee93cfd4b8e3a7b5740
alessandro <caprarelli.alessandro@gmail.com>
2015-03-12 11:07:17 +0100

    a

--- src/interfacce/Home.java
+++ src/interfacce/Home.java
@@ -13,8 +13,5 @@ public class Home {
     private JPanel ultimaGiornata;
     private JList list1;
 
-    private void createUIComponents() {
-        //ultimaGiornata = new JPanel();
-        ultimaGiornata.setBorder(new LineBorder(Color.GRAY, 10, true));
-    }
+
 }
