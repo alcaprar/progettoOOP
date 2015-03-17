10582d6e23662e4956d6bb97d42f8ab59d563cef
Manfredi Christian <c.manfredi.28@gmail.com>
2015-03-17 15:40:05 +0100

    piccola modifica

--- src/Fantacalcio.java
+++ src/Fantacalcio.java
@@ -9,7 +9,7 @@ import interfacce.*;
 
 public class Fantacalcio{
     public static void main(String args[]){
-        //Login logingui = new Login();
-        Applicazione a = new Applicazione();
+        Login logingui = new Login();
+        //Applicazione a = new Applicazione();
   	}
 }
