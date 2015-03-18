dd30d1c5be155362acba8f4e262188b5492c330c
Giacomo <gia25tiscali.it>
2015-03-18 16:15:27 +0100

    bozza delle classi giornata e formazione

---
+++ src/classi/Partita.java
@@ -0,0 +1,30 @@
+package classi;
+
+/**
+ * Created by Giacomo on 18/03/15.
+ */
+public class Partita {
+    private Formazione formCasa;
+    private Formazione formOspite;
+
+    public Partita(Formazione formCasa, Formazione formOspite) {bo
+        this.formCasa = formCasa;
+        this.formOspite = formOspite;
+    }
+
+    public Formazione getFormCasa() {
+        return formCasa;
+    }
+
+    public void setFormCasa(Formazione formCasa) {
+        this.formCasa = formCasa;
+    }
+
+    public Formazione getFormOspite() {
+        return formOspite;
+    }
+
+    public void setFormOspite(Formazione formOspite) {
+        this.formOspite = formOspite;
+    }
+}
