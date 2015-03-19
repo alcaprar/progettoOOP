638e200d24d046c19258c74ca7d52e9280abd53d
Manfredi Christian <c.manfredi.28@gmail.com>
2015-03-19 10:55:23 +0100

    piccole modifiche

--- src/interfacce/GestioneGiocatori.java
+++ src/interfacce/GestioneGiocatori.java
@@ -1,6 +1,10 @@
 package interfacce;
 
+import classi.Giocatore;
+
 import javax.swing.*;
+import javax.swing.text.StringContent;
+import java.util.ArrayList;
 
 /**
  * Created by Christian on 19/03/2015.
@@ -9,5 +13,25 @@ public class GestioneGiocatori extends JPanel{
 
 
     private JPanel mainPanel;
+    private JList listaGiocatori;
     private JList list1;
+
+    private DefaultListModel<String> listaG;
+    private ArrayList<Giocatore> giocatori;
+
+    public GestioneGiocatori(ArrayList<Giocatore> gioc){
+
+        giocatori = gioc;
+
+        creaListe();
+    }
+
+
+    private void createUIComponents() {
+        listaG = new DefaultListModel<String>();
+    }
+
+    private void creaListe(){
+        for(Giocatore g : giocatori) listaG.addElement(g.getCognome());
+    }
 }
