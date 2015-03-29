6fe78d7c5f9a82ee3d3af23d8ff86d6531dca6bb
alessandro <caprarelli.alessandro@gmail.com>
2015-03-29 10:11:18 +0200

    storico

---
+++ src/interfacce/Applicazione/Storico.java
@@ -0,0 +1,13 @@
+package interfacce.Applicazione;
+
+import javax.swing.*;
+
+/**
+ * Created by alessandro on 29/03/15.
+ */
+public class Storico {
+    private JComboBox edizioniComboBox;
+    private JTable classificaTable;
+    private JPanel mainPanel;
+    private JPanel calendarioPanel;
+}
